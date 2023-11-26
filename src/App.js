c00c7ff505b278a2bb0daabd69f5efc8ee761a41
codewithgi <118617546+codewgi@users.noreply.github.com>
2023-11-26 19:52:39 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="w-full min-h-screen bg-[#090909] grid place-content-center">
       <div className='w-[350px] h-[590px] bg-transparent cursor-pointer group rounded-3xl perspective-1000'>
-        <div className='relative preserve-3d group-hover:rotate-y-180 w-full h-full duration-500 '>
-          <div className='w-full h-full absolute  rounded-3xl overflow-hidden flex items-end'>
+        <div className='relative w-full h-full preserve-3d group-hover:rotate-y-180 duration-500'>
+          <div className='w-full h-full absolute rounded-3xl overflow-hidden'>
             <img src='/img/omen.jpeg' className='w-full h-full'/>
           </div>
-          <div className='absolute rotate-y-180 backface-hidden w-full h-full bg-[#0F1823] bg-opacity-95 overflow-hidden rounded-3xl space-y-5 p-10 text-neutral-300'>
+          <div className='absolute rotate-y-180 w-full h-full bg-[#0F1823] bg-opacity-95 rounded-3xl overflow-hidden p-10 text-neutral-300 space-y-5 backface-hidden'>
             <div>
               <span className='font-bold text-3xl'>AGENT | OMEN</span>
             </div>
@@ -17,8 +17,8 @@ function App() {
               <span className='font-semibold'>
                 // ROLE
               </span>
-              <span className='font-bold text-3xl'>
-              CONTROLLER
+              <span className='text-3xl font-bold'>
+                CONTROLLER 
               </span>
             </div>
             <div className='flex flex-col space-y-5'>
