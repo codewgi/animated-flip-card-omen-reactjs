650319b0297a3bd28350af650d203c3576a5c7b1
codewithgi <118617546+codewgi@users.noreply.github.com>
2023-11-26 19:53:04 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin");
+const plugin = require("tailwindcss/plugin")
 
 const CustomStyle = plugin(function ({addUtilities}) {
   addUtilities({
     ".rotate-y-180" : {
-      transform : "rotateY(180deg)",
+      transform : "rotateY(180deg)"
     },
     ".preserve-3d" : {
-      transformStyle : "preserve-3d",
+      transformStyle : "preserve-3d"
     },
     ".perspective-1000" : {
       perspective : "1000px"
     },
-    ".backface-hidden": {
-      backfaceVisibility: "hidden",
-    },
+    ".backface-hidden" : {
+      backfaceVisibility : "hidden",
+    }
   })
-});
+}) 
 
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
